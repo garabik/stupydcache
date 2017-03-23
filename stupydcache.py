6365ca0ced8cd052249c6d21b149f10bc548ab07
Radovan Garabík <radovan.garabik-github@kassiopeia.juls.savba.sk>
2017-03-23 16:49:26 +0100

    some python2 compatibility

--- stupydcache.py
+++ stupydcache.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 
 import os, zlib, pickle, base64, string, hashlib
 
+# for 2/3 compatibility
+try:
+    unicode
+except NameError:
+    unicode = ()
 
 class Cache:
     "general on-disk cache"
@@ -16,17 +22,21 @@ class Cache:
         self.name = name
 
     def _to_safe(self, s):
+        if isinstance(s, unicode): # for python3, it gives False, because unicode==()
+            s = s.encode('utf-8')  # but for python2, we have to use string, not unicode string
+        elif not isinstance(s, str):
+            s = str(s)
         return ''.join(x for x in s if x in string.ascii_letters+string.digits)
 
     def _fname(self, key):
         # asckey is used as a human readable prefix of the on-disk files, to aid in debugging
-        asckey = self._to_safe(str(key))
+        asckey = self._to_safe(key)
         key = repr(key)
         fname1 = asckey[:10]
-        h = hashlib.md5((self.name+key).encode('utf-8', errors='replace'))
+        h = hashlib.md5((repr(self.name)+key).encode('utf-8', errors='replace'))
         hd = h.digest()
         fname2 = base64.urlsafe_b64encode(hd)
-        fname2 = str(fname2, 'ascii')
+        fname2 = fname2.decode('ascii')
         fname = fname1 + fname2
         fname = os.path.join(self.cachedir, fname)
         return fname
@@ -53,12 +63,18 @@ class Cache:
 
 if __name__=='__main__':
     # test
-    cache = Cache(cachedir='/tmp/testcache', name='TEST')
-    k1 = 'kľúč1'
+    cache = Cache(cachedir='/tmp/testcache', name='TESTČ')
+    k1 = u'kľúč1'
     v1 = [1,2,3,4,5, 'šesť', b'sedem', {1,2,3}]
     cache[k1] = v1
     assert k1 in cache
     assert cache[k1] == v1
+    k2 = 123.45
+    v2 = 10
+    cache[k2] = v2
+    assert k2 in cache
+    assert cache[k2] == v2
+
     print(cache.get(k1,'ble'))
     print ('k2' in cache)
     print(cache['kľúč2']) # should fail with KeyError
